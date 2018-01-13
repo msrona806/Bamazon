46911b0d2fb317a98f4156c581f97d2b746d7da9
Sharona Young <youngs_e@hotmail.com>
2018-01-13 17:17:40 -0500

    added quit function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -46,21 +46,48 @@ function customerOrder() {
       message: "How many would you like?"
     }
   ])
+  // Take answers from user response:
   .then(function(userReq) {
-    updateStock(userReq.stockQty, userReq.itemId) 
-    products();
-    // connection.end();
-  })
-} 
+    // Pulling the item information for the customer selected item
+    leaveStore(userReq.itemId);
+    connection.query('SELECT * FROM products WHERE item_id = ?', [userReq.itemId], function(err, res) {
+          if (err) throw err;
+            var stockInv;
+            var itemCost;
+
+          for (i = 0; i < res.length; i++) {
+            stockInv = res[i].stock_quantity;
+            itemCost = res[i].price;
+          }
+          // Give an error msg if ordering more than in stock
+          if (stockInv < parseInt(userReq.stockQty)) {
+            console.log("You cannot buy more than we have! Choose again");
+            products();
+          }
+          // Shows customer their total for items ordered
+          else {
+            console.log("Your total is: " + userReq.stockQty * itemCost);
+            updateStock(userReq.stockQty, userReq.itemId)}
+          })
+          products();
+      })
+
+    }
 
 // Decrement stock from store inventory:
 function updateStock (qty, id) {
   connection.query('UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?', [qty, id], function(err, res){
-  })
+  });  
+} 
+
+function leaveStore(choice) {
+  if (choice == "Q") {
+    process.exit(0);
+  }
 }
-
-
-// don't let inventory go past 0
 // function to end connection, when user quits
+
+
+
 
 
