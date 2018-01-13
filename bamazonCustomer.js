888d7c505f530fd46c9edb0b0aeb2d25604a9c7f
Sharona Young <youngs_e@hotmail.com>
2018-01-13 14:05:19 -0500

    table updating quantities properly!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 // REQUIRED Packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var table = require("console.table");
 
 // connection to database
 var connection = mysql.createConnection({
@@ -18,11 +19,20 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  customerOrder();
+  products();
 });
 
+// **** FUNCTIONS ****
+
+// Display product table
+function products() {
+  connection.query('SELECT * FROM products', function(err, res) {
+    console.table(res);
+    customerOrder();
+  });
+}
+
 // Take customer order:
-// function
 function customerOrder() {
   inquirer.prompt([
     {
@@ -36,9 +46,21 @@ function customerOrder() {
       message: "How many would you like?"
     }
   ])
-} connection.end();
-
+  .then(function(userReq) {
+    updateStock(userReq.stockQty, userReq.itemId) 
+    products();
+    // connection.end();
+  })
+} 
 
 // Decrement stock from store inventory:
-// determine how many are in store inventory
+function updateStock (qty, id) {
+  connection.query('UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?', [qty, id], function(err, res){
+  })
+}
+
+
+// don't let inventory go past 0
+// function to end connection, when user quits
+
 
