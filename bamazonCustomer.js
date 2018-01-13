f9b9ebec670ad41c0b18fe26aa5da0005cc24c0f
Sharona Young <youngs_e@hotmail.com>
2018-01-13 00:45:30 -0500

    added connection for database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,27 @@
 // REQUIRED Packages
+var mysql = require("mysql");
 
 // connection to database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Sidley",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  readColleges();
+});
+
+// Take customer order
+
+// Decrement stock from store inventory
+
+
