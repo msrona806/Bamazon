43d0043d225230431b8298294eafd3f7e3e5fb85
Sharona Young <youngs_e@hotmail.com>
2018-01-14 00:47:56 -0500

    updating functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -47,47 +47,68 @@ function customerOrder() {
       name: "stockQty",
       type: "input",
       message: "How many would you like?"
-    }
-  ])
+    }])
   // Take answers from user response:
   .then(function(userReq) {
     // Pulling the item information for the customer selected item
     leaveStore(userReq.itemId);
     connection.query('SELECT * FROM products WHERE item_id = ?', [userReq.itemId], function(err, res) {
-          if (err) throw err;
-            var stockInv;
-            var itemCost;
+      if (err) throw err;
+        var stockInv;
+        var itemCost;
 
-          for (i = 0; i < res.length; i++) {
-            stockInv = res[i].stock_quantity;
-            itemCost = res[i].price;
-          }
-          // Give an error msg if ordering more than in stockgit 
-          if (stockInv < parseInt(userReq.stockQty)) {
-            console.log("You cannot buy more than we have! Choose again");
-            products();
-          }
-          // Shows customer their total for items ordered
-          else {
-            console.log(chalk.magentaBright("Your total is: ") + userReq.stockQty * itemCost);
-            updateStock(userReq.stockQty, userReq.itemId)}
-            products(); 
-          })
-        })
+      for (i = 0; i < res.length; i++) {
+        stockInv = res[i].stock_quantity;
+        itemCost = res[i].price;
       }
+      // Give an error msg if ordering more than in stockgit 
+      if (stockInv < parseInt(userReq.stockQty)) {
+        console.log(chalk.red("You cannot buy more than we have!"));
+        buyMore();
+      }
+      // Shows customer their total for items ordered
+      else {
+        console.log(chalk.magentaBright("Your total is: ") + userReq.stockQty * itemCost);
+        // updateStock(userReq.stockQty, userReq.itemId)
+        buyMore(); 
+
+      }  
+    })
+  })
+}
+
+// See if user wants to continue shopping
+function buyMore() {
+  inquirer.prompt(
+    {
+      name: "continue",
+      type: "confirm",
+      message: "Would you like to continue shopping?"
+    }
+  ).then(function(answer) {
+    // if the answer is yes, run customer order
+    if (answer.continue === true) {
+      updateStock();
+      products();
+    }
+    else {
+      process.exit(0);
+     }
+  });
+}
 
 // Decrement stock from store inventory:
 function updateStock (qty, id) {
   connection.query('UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?', [qty, id], function(err, res){
   });  
 } 
-
+// function to end connection, when user quits
 function leaveStore(choice) {
   if (choice == "Q") {
     process.exit(0);
   }
 }
-// function to end connection, when user quits
+
 
 
 
