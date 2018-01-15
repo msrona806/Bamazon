53c0b0aa0c8e06b4b6569042469c131c328e6d46
Sharona Young <youngs_e@hotmail.com>
2018-01-15 00:57:49 -0500

    started add producty function

--- bamazonManager.js
+++ bamazonManager.js
@@ -30,8 +30,8 @@ function menu() {
   inquirer.prompt ({
       name: 'menuItems',
       type: 'list',
-      message: (chalk.white('What would you like to do? Choose from an item from the list:')),
-      choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
+      message: (chalk.white('What would you like to do? Choose an option from the list:')),
+      choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', chalk.red('Exit')]
     })
   .then(function(mgrResp) {
     switch (mgrResp.menuItems) {
@@ -42,6 +42,10 @@ function menu() {
       case "View Low Inventory":
       lowInventory();
       break;
+
+      case "Add New Product":
+      addProduct();
+      break;
     } 
   });  
 }
@@ -51,17 +55,55 @@ function products() {
   chalkAnimation.rainbow("Welcome to Bamazon!!!", 1);
   connection.query('SELECT * FROM products', function(err, res) {
     console.table(res);
-    connection.end();
-    menu();
-  });
+      menu();
+  })
 }
 // View low inventory
 function lowInventory() {
   connection.query('SELECT * FROM products WHERE stock_quantity < 10 ', function(err, res) {
     console.table(res);
-    menu();
+      menu();
   })
 }
 
-
 // Add new product
+function addProduct() {
+  inquirer.prompt([
+    {
+      name: "itemId",
+      type: "input",
+      message: "What is the product Id?"
+    },
+    {
+      name: "product",
+      type: "input",
+      message: "What product are you adding?"
+    },
+    {
+      name: "dept",
+      type: "input",
+      message: "What department will it go in?"
+    },
+    {
+      name: "price",
+      type: "input",
+      message: "How much does it cost?"
+    },
+    {
+      name: "qty",
+      type: "input",
+      message: "How many are you adding?"
+    }
+  ])
+  .then(function(ans) {
+    // Add products to table
+    connection.query('INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES ?',
+      [ans.itemId, ans.product, ans.dept, ans.price, ans.qty],
+      
+      function(err, res) {
+        if (err) throw err;
+        console.log("added!");
+        menu();
+    })
+  });
+}
